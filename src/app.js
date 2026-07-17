319f89088e73cfc266bdd62a69155e5be3c7d5a6
Greenwich23 <nosaomo83@gmail.com>
2026-07-17 09:34:30 +0100

    added frontend url

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ app.use(
     origin: [
       process.env.CUSTOMER_URL || "http://localhost:5173",
       process.env.ADMIN_URL || "http://localhost:3001",
+      "https://apexsportsfitness.netlify.app/",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
